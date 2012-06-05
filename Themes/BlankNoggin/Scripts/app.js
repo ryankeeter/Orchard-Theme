927f19a2abbece29cba5620b65cadfc01d87a283
Ryan J. Keeter <ryan.keeter@gmail.com>
2012-06-05 01:00:33 -0500

    finished the home page, moving onto the content pages

--- Themes/BlankNoggin/Scripts/app.js
+++ Themes/BlankNoggin/Scripts/app.js
@@ -1,12 +1,16 @@
  $(document).ready(function(){
-
-   
-    
     function FirstPostContentOver(){
             $("#firstPostContent").show("slide", {direction: "up"}, 250);
     }
     function FirstPostContentOut(){
              $("#firstPostContent").hide("slide", {direction: "down"}, 250);
     }
+     function SecondPostContentOver(){
+            $("#secondPostContent").show("slide", {direction: "up"}, 250);
+    }
+    function SecondPostContentOut(){
+             $("#secondPostContent").hide("slide", {direction: "down"}, 250);
+    }
     $("#firstPost").hoverIntent(FirstPostContentOver, FirstPostContentOut);
+    $("#secondPost").hoverIntent(SecondPostContentOver, SecondPostContentOut);
 });
