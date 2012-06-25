fb74c785b7e67a6898eedd3c3b97ea577b49aa74
Ryan J. Keeter <ryan.keeter@gmail.com>
2012-06-25 14:21:39 -0500

    Finished Most of it, just tweaking now

--- Themes/BlankNoggin/Scripts/app.js
+++ Themes/BlankNoggin/Scripts/app.js
@@ -1,16 +1,28 @@
- $(document).ready(function(){
-    function FirstPostContentOver(){
-            $("#firstPostContent").show("slide", {direction: "up"}, 250);
+$(document).ready(function() {
+    function FirstPostContentOver() {
+        $("#firstPostContent").show("slide", { direction: "up" }, 250);
     }
-    function FirstPostContentOut(){
-             $("#firstPostContent").hide("slide", {direction: "down"}, 250);
+    function FirstPostContentOut() {
+        $("#firstPostContent").hide("slide", { direction: "down" }, 250);
     }
-     function SecondPostContentOver(){
-            $("#secondPostContent").show("slide", {direction: "up"}, 250);
+    function SecondPostContentOver() {
+        $("#secondPostContent").show("slide", { direction: "up" }, 250);
     }
-    function SecondPostContentOut(){
-             $("#secondPostContent").hide("slide", {direction: "down"}, 250);
+    function SecondPostContentOut() {
+        $("#secondPostContent").hide("slide", { direction: "down" }, 250);
     }
     $("#firstPost").hoverIntent(FirstPostContentOver, FirstPostContentOut);
     $("#secondPost").hoverIntent(SecondPostContentOver, SecondPostContentOut);
+
+    //remove Disqus styling
+    $("#comments style").remove();
+    $("#dsq-combo-widget").attr("class", "custom");
+    $("#dsq-combo-tabs").remove();
+    $("#dsq-combo-people h3").remove();
+
+    //load external favicons onto links
+    $("a[href^='http']").each(function() {
+        $(this).css({"background": "url(http://g.etfv.co/" + this.href + ") right center no-repeat",  "padding-right": "40px"});              
+        });
+    
 });
